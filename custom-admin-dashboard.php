daf64fe2a4047d6248f29fe18c7433808a56326c
V Mishan Tharindu <v.mishantharindu@gmail.com>
2025-12-21 12:23:11 +0530

    change plugin version

--- custom-admin-dashboard.php
+++ custom-admin-dashboard.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Custom Admin Dashboard
  * Description: A custom plugin to modify and clean up the WordPress admin dashboard.
- * Version: 1.6.0
+ * Version: 1.6.1
  * Author: TechM
  * Author URI: https://yourwebsite.com
  * Text Domain: custom-admin-dashboard
