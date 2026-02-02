9d7fed145f4f8a5e1b63ecc46b81f3dba33b7650
V Mishan Tharindu <v.mishantharindu@gmail.com>
2026-02-03 01:06:55 +0530

    change version to 2.0.3

--- custom-admin-dashboard.php
+++ custom-admin-dashboard.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Custom Admin Dashboard
  * Description: A custom plugin to modify and clean up the WordPress admin dashboard. [wwmt_time_ago] or [wwmt_time_ago icon="clock"], [post_image_count], [date_weather], [wwmt_ad space_id="wwmt-advertisment-space-01"], [wwmt_ad space_id="wwmt-advertisment-space-02"], [wwmt_ad space_id="wwmt-advertisment-space-03"], [wwmt_ad space_id="wwmt-advertisment-space-04"], [wwmt_ad space_id="wwmt-advertisment-space-05"],[post_reactions]
- * Version: 2.0.2
+ * Version: 2.0.3
  * Author: TechM
  * Author URI: https://yourwebsite.com
  * Text Domain: custom-admin-dashboard
